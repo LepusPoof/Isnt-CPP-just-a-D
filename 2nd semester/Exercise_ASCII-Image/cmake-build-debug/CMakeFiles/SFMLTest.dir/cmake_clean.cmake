4a8b6dfafaa8ac333e9666783e01f16fa4df9610
LepusPoof <64106901+LepusPoof@users.noreply.github.com>
2021-06-14 00:59:11 +0200

    017.

---
+++ 2nd semester/Exercise_ASCII-Image/cmake-build-debug/CMakeFiles/SFMLTest.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SFMLTest.dir/main.cpp.obj"
+  "SFMLTest.exe"
+  "SFMLTest.exe.manifest"
+  "SFMLTest.pdb"
+  "libSFMLTest.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SFMLTest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ 2nd semester/Exercise_ASCII-Image/cmake-build-debug/CMakeFiles/SFMLTest.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for SFMLTest.
+# This may be replaced when dependencies are built.
